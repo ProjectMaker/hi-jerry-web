d41e011f09e071ad92e011fb3e71528156b060d8
Thomas MICHELET <thomas@mobilispro.com>
2017-09-07 06:21:46 +0200

    Remove webpack

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ if (isDevelopment) {
   }));
   app.use(webpackHotMiddleware(compiler));
 } else {
-  app.use(express.static(DIST_DIR));
+  //app.use(express.static(DIST_DIR));
 
-  app.get("*", (req, res) => res.sendFile(HTML_FILE));
+  //app.get("*", (req, res) => res.sendFile(HTML_FILE));
 }
 
 
@@ -28,5 +28,5 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-
+console.log('************************* port', port);
 app.listen(port);
