9d8c3cb72c9a443c9000705d85a545dc5c802d12
Thomas MICHELET <thomas@mobilispro.com>
2017-09-26 17:28:45 +0200

    [#13] navbar

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import '../assets/css/styles.css';
+import { AuthenticationService } from './shared/authentication/services/authentication.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  protected showNav:boolean = false;
+
+  public constructor(private auth:AuthenticationService, private router:Router) {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe((event:NavigationEnd) => {
+        this.showNav = ['/front/account/signin','/front/account/register','/front/account/unauthorized'].indexOf(event.url) === -1;
+      });
+  }
+  
+  protected logout() {
+    this.auth.logout();
+    this.router.navigate(['/front/account/signin']);
+  }
+}
