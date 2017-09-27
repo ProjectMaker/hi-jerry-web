1b80de91d6487df88066e51524a770922a1fc67b
Thomas MICHELET <thomas@mobilispro.com>
2017-09-27 16:19:52 +0200

    [#14] use reducer

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { Store } from '@ngrx/store';
+import 'rxjs/add/operator/let';
 
 import '../assets/css/styles.css';
 import { AuthenticationService } from './shared/authentication/services/authentication.service';
+import { AppState } from './shared/store';
+import { getIsUserSignedIn$ } from './shared/store/account/account.selectors';
 
 @Component({
   selector: 'my-app',
@@ -10,15 +14,9 @@ import { AuthenticationService } from './shared/authentication/services/authenti
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  protected showNav:boolean = false;
+  protected isSignedIn$ = this.store.let(getIsUserSignedIn$);
 
-  public constructor(private auth:AuthenticationService, private router:Router) {
-    this.router.events
-      .filter(event => event instanceof NavigationEnd)
-      .subscribe((event:NavigationEnd) => {
-        this.showNav = ['/front/account/signin','/front/account/register','/front/account/unauthorized'].indexOf(event.url) === -1;
-      });
-  }
+  public constructor(private auth:AuthenticationService, private router:Router, private store:Store<AppState>) { }
   
   protected logout() {
     this.auth.logout();
